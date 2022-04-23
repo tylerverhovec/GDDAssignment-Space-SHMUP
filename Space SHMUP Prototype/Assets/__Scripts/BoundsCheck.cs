2e26b3a2209bcdf15f315a81a615ee889f85c8e0
Tyler V <verhovectyler@gmail.com>
2022-04-22 21:08:11 -0600

    Added basic movement to Enemy_0

--- Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
+++ Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
@@ -10,10 +10,15 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour {
     [Header("Set in Inspector")]
     public float    radius = 1f;
+    public bool     keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool     isOnScreen = true;
     public float    camWidth;
     public float    camHeight;
+
+    [HideInInspector]                                    
+    public bool     offRight, offLeft, offUp, offDown;
 
     void Awake() {
         camHeight = Camera.main.orthographicSize;
@@ -22,23 +27,38 @@ public class BoundsCheck : MonoBehaviour {
 
     void LateUpdate () {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
         if (pos.x > camWidth - radius) {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
 
         if (pos.x < -camWidth + radius) {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
 
         if (pos.y > camHeight - radius) {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
         if (pos.y < -camHeight + radius) {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
         }
 
-        transform.position = pos;
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+        if ( keepOnScreen && !isOnScreen ) {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
     }
 
     // Draw the bounds in the Scene pane using OnDrawGizmos()
