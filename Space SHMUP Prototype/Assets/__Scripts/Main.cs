127d6e25a3044bbd3c48c707a8e92614e6fab577
Tyler V <verhovectyler@gmail.com>
2022-04-22 21:53:48 -0600

    Game now restarts after player is destroyed

--- Space SHMUP Prototype/Assets/__Scripts/Main.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Main.cs
@@ -44,4 +44,15 @@ public class Main : MonoBehaviour {
         // Invoke SpawnEnemy() again
         Invoke( "SpawnEnemy", 1f/enemySpawnPerSecond );
     }
+
+    public void DelayedRestart( float delay ) {
+        // Invoke the Restart() method in delay seconds
+        Invoke( "Restart", delay );
+    }
+
+    public void Restart() {
+        // Reload _Scene_0 to restart the game
+        SceneManager.LoadScene( "_Scene_0");
+    }
+
 }
