6020dfb7c54f5bb243b8593db42f3bb281780384
Tyler V <verhovectyler@gmail.com>
2022-04-22 21:51:11 -0600

    Enemies colliding with player damages player

--- Space SHMUP Prototype/Assets/__Scripts/Hero.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -12,28 +12,66 @@ public class Hero : MonoBehaviour {
     public float            pitchMult = 30;
 
     [Header("Set Dynamically")]
-    public float            shieldLevel = 1;
+    [SerializeField]
+    private float           _shieldLevel = 1;
+
+    // This variable holds a reference to the last triggering GameObject
+    private GameObject       lastTriggerGo = null;
 
     void Awake() {
         if (S == null) {
-        S = this; // Set the Singleton
-    } else {
-        Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+            S = this; // Set the Singleton
+        }    
+        else {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+    void Update () {
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
+      }
+
+    void OnTriggerEnter(Collider other) {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        //print("Triggered: "+go.name);
+
+        // Make sure it's not the same triggering go as last time
+        if (go == lastTriggerGo) {
+            return;
+        }
+        lastTriggerGo = go;
+
+        if (go.tag == "Enemy") {  // If the shield was triggered by an enemy
+            shieldLevel--;        // Decrease the level of the shield by 1
+            Destroy(go);          // … and Destroy the enemy
+        } else {
+            print("Triggered by non-Enemy: "+go.name);
+        }
+    }
+
+    public float shieldLevel {
+        get {
+            return( _shieldLevel );
+        }
+        set {
+            _shieldLevel = Mathf.Min( value,4 );
+            // If the shield is going to be set to less than zero
+            if (value < 0) {
+                Destroy(this.gameObject);
+            }
+        }
     }
-}
 
-void Update () {
-    // Pull in information from the Input class
-    float xAxis = Input.GetAxis("Horizontal");
-    float yAxis = Input.GetAxis("Vertical");
-
-    // Change transform.position based on the axes
-    Vector3 pos = transform.position;
-    pos.x += xAxis * speed * Time.deltaTime;
-    pos.y += yAxis * speed * Time.deltaTime;
-    transform.position = pos;
-
-    // Rotate the ship to make it feel more dynamic
-    transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
-  }
 }
